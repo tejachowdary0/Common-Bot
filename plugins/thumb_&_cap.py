03cc690bf8ee43bf6765ab2a16debadaaa7c4c86
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-22 12:18:20 +0530

    Update thumb_&_cap.py

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -13,36 +13,36 @@ async def add_caption(client, message):
 async def delete_caption(client, message):
     caption = await db.get_caption(message.from_user.id)  
     if not caption:
-       return await message.reply_text("__**😔 Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴy Cᴀᴩᴛɪᴏɴ**__")
+       return await message.reply_text("__**😔 You Don't have Any Caption**__")
     await db.set_caption(message.from_user.id, caption=None)
-    await message.reply_text("__**❌️ Cᴀᴩᴛɪᴏɴ Dᴇʟᴇᴛᴇᴅ**__")
+    await message.reply_text("__**❌️ Caption Deleted**__")
                                        
-@Client.on_message(filters.private & filters.command(['see_caption', 'view_caption']))
+@Client.on_message(filters.private & filters.command(['see_caption']))
 async def see_caption(client, message):
     caption = await db.get_caption(message.from_user.id)  
     if caption:
-       await message.reply_text(f"**Yᴏᴜ'ʀᴇ Cᴀᴩᴛɪᴏɴ:-**\n\n`{caption}`")
+       await message.reply_text(f"**You're Caption :-**\n\n`{caption}`")
     else:
-       await message.reply_text("__**😔 Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴy Cᴀᴩᴛɪᴏɴ**__")
+       await message.reply_text("__**😔 You Don't have Any Caption**__")
 
 
-@Client.on_message(filters.private & filters.command(['view_thumb', 'viewthumb']))
+@Client.on_message(filters.private & filters.command(['showthumbnail']))
 async def viewthumb(client, message):    
     thumb = await db.get_thumbnail(message.from_user.id)
     if thumb:
        await client.send_photo(chat_id=message.chat.id, photo=thumb)
     else:
-        await message.reply_text("😔 __**Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴy Tʜᴜᴍʙɴᴀɪʟ**__") 
+        await message.reply_text("😔 __**You Don't have Any Thumbnail**__") 
 		
-@Client.on_message(filters.private & filters.command(['del_thumb', 'delthumb']))
+@Client.on_message(filters.private & filters.command(['deletethumbnail']))
 async def removethumb(client, message):
     await db.set_thumbnail(message.from_user.id, file_id=None)
-    await message.reply_text("❌️ __**Tʜᴜᴍʙɴᴀɪʟ Dᴇʟᴇᴛᴇᴅ**__")
+    await message.reply_text("❌️ __**Your Thumbnail Deleted Successfully 🗑️**__")
 	
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client, message):
-    mkn = await message.reply_text("Please Wait ...")
+    mkn = await message.reply_text("**Please Wait...**")
     await db.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
-    await mkn.edit("✅️ __**Tʜᴜᴍʙɴᴀɪʟ Sᴀᴠᴇᴅ**__")
+    await mkn.edit("✅️ __**Your Thumbnail Saved Permanently**__")
 
 
