84c2a9a3814bee4f7eb901f3d37bd0ea8723ed81
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-23 17:57:34 +0530

    Create callback.py

---
+++ plugins/callback.py
@@ -0,0 +1,18 @@
+from pyrogram import Client, filters
+from helper.database import db
+
+
+
+
+
+
+
+
+
+
+@Client.on_callback_query()
+async def cb_handlers(c: Client, cb: "types.CallbackQuery"):
+    elif cb.data == "deleteThumbnail":
+        await db.set_thumbnail(cb.from_user.id, file_id=None)
+        await cb.answer("❌️ __**Your Thumbnail Deleted Successfully 🗑️**__", show_alert=True)
+        await cb.message.delete(True)
