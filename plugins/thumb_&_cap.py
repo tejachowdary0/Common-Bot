1fa527115286a6324c760b671564c87864f6e938
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-05-23 17:59:32 +0530

    Update thumb_&_cap.py

--- plugins/thumb_&_cap.py
+++ plugins/thumb_&_cap.py
@@ -51,7 +51,7 @@ async def addthumbs(client, message):
 
 @Client.on_callback_query()
 async def cb_handlers(c: Client, cb: "types.CallbackQuery"):
-    elif cb.data == "deleteThumbnail":
+    if cb.data == "deleteThumbnail":
         await db.set_thumbnail(cb.from_user.id, file_id=None)
         await cb.answer("❌️ __**Your Thumbnail Deleted Successfully 🗑️**__", show_alert=True)
         await cb.message.delete(True)
