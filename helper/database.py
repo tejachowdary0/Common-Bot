231662dc2eb703d02488fe5c55f4e950d78f7dfd
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-11-28 17:47:47 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -43,7 +43,7 @@ class Database:
         await self.col.delete_many({'_id': int(user_id)})
 
     async def set_upload_mode(self, id, upload_mode):
-        await self.col.update_one({'_id': id}, {'$set': {'upload_mode': upload_mode}})
+        await self.col.update_one({'id': id}, {'$set': {'upload_mode': upload_mode}})
 
     async def get_upload_mode(self, id):
         user = await self.col.find_one({'_id': int(id)})
